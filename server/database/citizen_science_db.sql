178752742774134eca305a6ab07ec52f906871de
Paldin Bet Eivaz <beteivap@oregonstate.edu>
2022-01-22 14:50:10 -0800

    removed unnecessary sql file

--- server/database/citizen_science_db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE teachers (
-    teacher_id SERIAL PRIMARY KEY,
-    email VARCHAR UNIQUE NOT NULL,
-    password VARCHAR NOT NULL,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    school VARCHAR NOT NULL,
-    date_joined DATE NOT NULL
-);
