71c578407eec47c299dfd7c7e0e85d0e61961348
Paldin Bet Eivaz <beteivap@oregonstate.edu>
2022-02-02 21:28:47 -0800

    created/updated project routes, removed teacher route

--- server/app.py
+++ server/app.py
@@ -1,67 +1,119 @@
 import os
-
 from datetime import date
 from dotenv import load_dotenv, find_dotenv
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from flask_migrate import Migrate
+
 
 load_dotenv(find_dotenv())
-host = os.environ.get('HOST')
-user = os.environ.get('USER')
-pwd = os.environ.get('PWD')
-db_name = os.environ.get('DB')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{pwd}@{host}:5432/{db_name}'
+env_config = os.getenv('APP_SETTINGS', 'config.DevelopmentConfig')
+app.config.from_object(env_config)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
-migrate = Migrate(app, db)
 
 from models import *
 from schemas import *
 
 
-@app.route('/auth/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        if request.is_json:
-            new_teacher = TeachersModel(
-                email=request.json['email'],
-                password=request.json['password'],
-                first_name=request.json['firstname'],
-                last_name=request.json['lastname'],
-                school=request.json['school'],
-                date_joined=date.today()
-                )
-            db.session.add(new_teacher)
-            db.session.commit()
-            return teacher_schema.jsonify(new_teacher)
-        else:
-            return jsonify({'error': 'Data must be JSON formatted!'})
+# Returns a list of projects for teacher with id <teacher_id>
+@app.route('/projects/<int:teacher_id>', methods=['GET'])
+def get_projects(teacher_id):
+    projects = Project.query.filter(Project.teacher_id == teacher_id)
+
+    return projects_schema.jsonify(projects)
 
 
-@app.route('/projects/create/<teacher_id>', methods=['POST'])
+# Returns info for project with id <project_id>
+@app.route('/projects/info/<int:project_id>', methods=['GET'])
+def get_project(project_id):
+    project_info = db.session.query(Project, IVInfo, DVInfo) \
+                   .join(IVInfo, IVInfo.iv_info_id == Project.iv_info_id) \
+                   .join(DVInfo, DVInfo.dv_info_id == Project.dv_info_id) \
+                   .filter(Project.project_id == project_id).one()
+
+    return projects_schema.jsonify(project_info)
+
+
+# Updates project with id <project_id>
+@app.route('/projects/info/<int:project_id>', methods=['PUT'])
+def update_project(project_id):
+    db.session.query(Project).filter_by(project_id=project_id).update(request.json)
+    db.session.commit()
+
+    return project_schema.jsonify(Project.query.get(project_id))
+
+
+# Deletes project with id <project_id>
+@app.route('/projects/info/<int:project_id>', methods=['DELETE'])
+def delete_project(project_id):
+    project = Project.query.get(project_id)
+    db.session.delete(project)
+    db.session.commit()
+
+    return project_schema.jsonify(project)
+
+
+def create_iv_info(request):
+    """Adds a new entry to IVInfo table and returns its id"""
+    new_iv_info = IVInfo(
+        iv_name=request.json['iv_name'],
+        iv_type=request.json['iv_type'],
+        iv_units=request.json['iv_units'],
+        iv_accepted=request.json['iv_accepted']
+    )
+
+    db.session.add(new_iv_info)
+    db.session.commit()
+
+    return new_iv_info.iv_info_id
+
+
+def create_dv_info(request):
+    """Adds a new entry to DVInfo table and returns its id"""
+    new_dv_info = DVInfo(
+        dv_name=request.json['dv_name'],
+        dv_type=request.json['dv_type'],
+        dv_units=request.json['dv_units'],
+        dv_accepted=request.json['dv_accepted']
+    )
+
+    db.session.add(new_dv_info)
+    db.session.commit()
+
+    return new_dv_info.dv_info_id
+
+
+# Creates a new project for teacher with id <teacher_id>
+@app.route('/projects/create/<int:teacher_id>', methods=['POST'])
 def create_project(teacher_id):
-    if request.method == 'POST':
-        if request.is_json:
-            new_project = ProjectsModel(
-                teacher_id=teacher_id,
-                project_code=request.json['project_code'],
-                title=request.json['title'],
-                description=request.json['description'],
-                prompt=request.json['prompt'],
-                category=request.json['category'],
-                end_date=request.json['end_date']
-            )
-            db.session.add(new_project)
-            db.session.commit()
-            return project_schema.jsonify(new_project)
-        else:
-            return jsonify({'error': 'Data must be JSON formatted!'})
+    if request.is_json:
+        new_project = Project(
+            teacher_id=teacher_id,
+            project_code='XXXX-X-X',
+            title=request.json['title'],
+            description=request.json['description'],
+            prompt=request.json['prompt'],
+            category=request.json['category'],
+            end_date=request.json['end_date'],
+            iv_info_id=create_iv_info(request),
+            dv_info_id=create_dv_info(request)
+        )
+
+        db.session.add(new_project)
+        db.session.commit()
+        new_project.project_code = (
+            f'{date.today().year}-{teacher_id}-{new_project.project_id}'
+        )
+        db.session.commit()
+
+        return project_schema.jsonify(new_project)
+    else:
+        return jsonify(error='Data must be JSON formatted!')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
