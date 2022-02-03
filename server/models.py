364de138b9e17991f4a0944e8c3f32601047926b
Paldin Bet Eivaz <beteivap@oregonstate.edu>
2022-02-02 21:15:41 -0800

    added IVInfo, DVInfo, Observation models, removed Teachers model

--- server/models.py
+++ server/models.py
@@ -1,44 +1,30 @@
 from app import db
+from sqlalchemy.dialects.postgresql import JSON
 
 
-class TeachersModel(db.Model):
-    __tablename__ = 'teachers'
-
-    teacher_id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(), unique=True, nullable=False)
-    password = db.Column(db.String(), nullable=False)
-    first_name = db.Column(db.String(), nullable=False)
-    last_name = db.Column(db.String(), nullable=False)
-    school = db.Column(db.String(), nullable=False)
-    date_joined = db.Column(db.Date(), nullable=False)
-
-    def __init__(self, email, password, first_name, last_name, school, date_joined):
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
-        self.school = school
-        self.date_joined = date_joined
-
-    def __repr__(self):
-        return f'Teacher {self.first_name} {self.last_name} {self.school}'
-
-
-class ProjectsModel(db.Model):
+class Project(db.Model):
     __tablename__ = 'projects'
 
     project_id = db.Column(db.Integer, primary_key=True)
-    teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.teacher_id'))
+    teacher_id = db.Column(db.Integer, nullable=False)
     project_code = db.Column(db.String(), unique=True, nullable=False)
     title = db.Column(db.String(), nullable=False)
     description = db.Column(db.Text(), nullable=False)
     prompt = db.Column(db.Text(), nullable=False)
     category = db.Column(db.String(), nullable=False)
     end_date = db.Column(db.Date(), nullable=False)
+    iv_info_id = db.Column(
+        db.Integer, db.ForeignKey('ivinfo.iv_info_id', ondelete='CASCADE')
+    )
+    dv_info_id = db.Column(
+        db.Integer, db.ForeignKey('dvinfo.dv_info_id', ondelete='CASCADE')
+    )
 
-    teachers = db.relationship('TeachersModel')
+    iv_info = db.relationship('IVInfo', cascade='all, delete')
+    dv_info = db.relationship('DVInfo', cascade='all, delete')
 
-    def __init__(self, teacher_id, project_code, title, description, prompt, category, end_date):
+    def __init__(self, teacher_id, project_code, title, description,
+                 prompt, category, end_date, iv_info_id, dv_info_id):
         self.teacher_id = teacher_id
         self.project_code = project_code
         self.title = title
@@ -46,6 +32,67 @@ class ProjectsModel(db.Model):
         self.prompt = prompt
         self.category = category
         self.end_date = end_date
+        self.iv_info_id = iv_info_id
+        self.dv_info_id = dv_info_id
 
     def __repr__(self):
         return f'Project {self.project_code} {self.title}'
+
+
+class IVInfo(db.Model):
+    __tablename__ = 'ivinfo'
+
+    iv_info_id = db.Column(db.Integer, primary_key=True)
+    iv_name = db.Column(db.String(), nullable=False)
+    iv_type = db.Column(db.String(), nullable=False)
+    iv_units = db.Column(db.String())
+    iv_accepted = db.Column(JSON)
+
+    def __init__(self, iv_name, iv_type, iv_units, iv_accepted):
+        self.iv_name = iv_name
+        self.iv_type = iv_type
+        self.iv_units = iv_units
+        self.iv_accepted = iv_accepted
+
+    def __repr__(self):
+        return f'IVInfo {self.iv_name} {self.iv_type}'
+
+
+class DVInfo(db.Model):
+    __tablename__ = 'dvinfo'
+
+    dv_info_id = db.Column(db.Integer, primary_key=True)
+    dv_name = db.Column(db.String(), nullable=False)
+    dv_type = db.Column(db.String(), nullable=False)
+    dv_units = db.Column(db.String())
+    dv_accepted = db.Column(JSON)
+
+    def __init__(self, dv_name, dv_type, dv_units, dv_accepted):
+        self.dv_name = dv_name
+        self.dv_type = dv_type
+        self.dv_units = dv_units
+        self.dv_accepted = dv_accepted
+
+    def __repr__(self):
+        return f'DVInfo {self.dv_name} {self.dv_type}'
+
+
+class Observation(db.Model):
+    __tablename__ = 'observations'
+
+    obs_id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey('projects.project_id'))
+    device_id = db.Column(db.String(), nullable=False)
+    obs_vals = db.Column(JSON, nullable=False)
+    obs_date = db.Column(db.Date(), nullable=False)
+
+    projects = db.relationship('Project')
+
+    def __init__(self, project_id, device_id, obs_vals, obs_date):
+        self.project_id = project_id
+        self.device_id = device_id
+        self.obs_vals = obs_vals
+        self.obs_date = obs_date
+
+    def __repr__(self):
+        return f'Observation {self.device_id} {self.obs_vals}'
