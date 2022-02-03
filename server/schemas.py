31154f74eb94ce79174313526b90a82039b8e001
Paldin Bet Eivaz <beteivap@oregonstate.edu>
2022-02-02 21:19:30 -0800

    updated Project, deleted Teacher, created Observation schemas

--- server/schemas.py
+++ server/schemas.py
@@ -1,23 +1,24 @@
 from app import ma
 
 
-class TeacherSchema(ma.Schema):
-    class Meta:
-        fields = (
-            'teacher_id', 'email', 'password',
-            'first_name', 'last_name', 'school', 'date_joined'
-            )
-
-teacher_schema = TeacherSchema()
-teachers_schema = TeacherSchema(many=True)
-
-
 class ProjectSchema(ma.Schema):
     class Meta:
         fields = (
-            'project_id', 'teacher_id', 'project_code',
-            'title', 'description', 'prompt', 'category', 'end_date'
-            )
+            'project_id', 'teacher_id', 'project_code', 'title',
+            'description', 'prompt', 'category', 'end_date',
+            'iv_info_id', 'iv_name', 'iv_type', 'iv_units', 'iv_accepted',
+            'dv_info_id', 'dv_name', 'dv_type', 'dv_units', 'dv_accepted'
+        )
 
 project_schema = ProjectSchema()
 projects_schema = ProjectSchema(many=True)
+
+
+class ObservationSchema(ma.Schema):
+    class Meta:
+        fields = (
+            'obs_id', 'project_id', 'device_id', 'obs_vals', 'obs_date'
+        )
+
+observation_schema = ObservationSchema()
+observations_schema = ObservationSchema(many=True)
