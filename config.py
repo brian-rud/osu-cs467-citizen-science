7706e20af4de58c089b8b68070073ac92f734299
Paldin Bet Eivaz <beteivap@oregonstate.edu>
2022-02-05 12:59:03 -0800

    resolving SQLAlchemy config params to work on Heroku

--- config.py
+++ config.py
@@ -4,7 +4,9 @@ import os
 class Config(object):
     DEBUG = False
     DEVELOPMENT = False
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = (
+        os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
