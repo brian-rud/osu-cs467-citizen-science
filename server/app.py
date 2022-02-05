f37278fb9e12a6b88f4fb144570ab04053cd733d
Paldin Bet Eivaz <beteivap@oregonstate.edu>
2022-02-04 21:37:31 -0800

    refactored app config

--- server/app.py
+++ server/app.py
@@ -4,13 +4,12 @@ from dotenv import load_dotenv, find_dotenv
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from .config_app import *
 
 
 load_dotenv(find_dotenv())
 
 app = Flask(__name__)
-app.config.from_object(os.getenv('APP_SETTINGS', 'config_app.DevelopmentConfig'))
+app.config.from_envvar('APP_SETTINGS')
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
