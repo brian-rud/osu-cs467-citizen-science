e7411a98b1390649e735004a5d11ce55b317a262
Paldin Bet Eivaz <beteivap@oregonstate.edu>
2022-02-04 20:40:28 -0800

    moved app config to server subdirectory

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv, find_dotenv
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from config_app import *
+from server.config_app import *
 
 
 load_dotenv(find_dotenv())
